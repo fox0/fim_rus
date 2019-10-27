44140d59275f35f623f01c1fbae4900e36e073b9
fox0 <red-fox0@yandex.ru>
2019-10-27 18:44:01 +0700

    up

--- FimRus.g4
+++ FimRus.g4
@@ -3,29 +3,51 @@ grammar FimRus;
 program:
     'Дорогая принцесса Селестия' DOT
     body
-    ('Ваша верная ученица'|'Ваш верный ученик') COMMA author DOT;
-
-author:
-    ID+;
+    ('Ваша верная ученица'|'Ваш верный ученик') COMMA variable DOT;
 
 body:
-    statements DOT
+    statements endLine
     ;
 
 statements:
-    statement (DOT statement)*;
+    statement (endLine statement)*;
 
-statement:
-    say
+endLine:
+    DOT | QUESTION;
+
+statement
+    : say
+    | assignmentStatement
     ;
 
 say:
-    'Я сказал' 'а'? CONST_STR;
+    'Я сказал' 'а'? expressions;
+
+assignmentStatement:
+    'Вы знали, что' variable 'нравится' expression;
+
+expressions:
+    expression+;
+
+expression
+    : 'число' CONST_INT
+    | CONST_STR
+    | variable
+    ;
+
+variable:
+    ID+;
 
 DOT: '.';
 COMMA: ',';
+QUESTION: '?';
+//COLON: ':';
+
+//MINUS: '-';
 
 ID: [а-яА-ЯёЁ]+;
+CONST_INT: [0-9]+;
 CONST_STR: '"' (~ ('"'))* '"';
+
 WS: [ \t\r\n]+ -> skip;
 COMMENT: '(' .*? ')' -> skip;
--- fim_rus.py
+++ fim_rus.py
@@ -1,4 +1,5 @@
 import sys
+import logging
 
 from antlr4.FileStream import FileStream
 from antlr4.CommonTokenStream import CommonTokenStream
@@ -8,13 +9,42 @@ from FimRusLexer import FimRusLexer
 from FimRusListener import FimRusListener
 from FimRusParser import FimRusParser
 
+log = logging.getLogger(__name__)
+
 
 class Listener(FimRusListener):
     def __init__(self):
         self.vars = {}
 
+    def exitAssignmentStatement(self, ctx: FimRusParser.AssignmentStatementContext):
+        var = ctx.variable().getText()
+        expr = ctx.expression()
+        value = self._parse_expr(expr)
+        log.debug('%s = %d', var, value)
+        self.vars[var] = value
+
     def exitSay(self, ctx: FimRusParser.SayContext):
-        print(ctx.CONST_STR().getText().strip('"'))
+        for expr in ctx.expressions().children:
+            r = self._parse_expr(expr)
+            if isinstance(r, str):
+                print(r, end='')
+            else:
+                log.error('NotImplementedError')
+        print()
+
+    @staticmethod
+    def _parse_expr(expr):
+        log.debug(expr.getText())
+
+        const_int = expr.CONST_INT()
+        if const_int:
+            return int(const_int.getText())
+
+        const_str = expr.CONST_STR()
+        if const_str:
+            return const_str.getText().strip('"')
+
+        log.error('NotImplementedError')
 
 
 def main(filename):
@@ -28,7 +58,9 @@ def main(filename):
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
     if len(sys.argv) == 2:
         main(sys.argv[1])
     else:
         main('examples/00-hello.fim')
+        # main('examples/01-99bottles.fim')
